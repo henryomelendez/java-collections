b4b13dcb7ac27cc48ad1a5df150edd2af79fbd60
Melendez <henryomar.melendez@fiserv.com>
2023-10-23 12:21:49 -0400

    finished ex2 and ex3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,25 +5,27 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        ex1();
+        ex3();
     }
-
     private static void ex1() { // Range of people
         // TODO...
         List<Person> personList = DataRepo.getPeople();
         personList = personList.stream().filter(x-> x.getAge() >= 20 && x.getAge() <= 40).collect(Collectors.toList());
         System.out.println(personList.toString());
-
     }
-
     private static void ex2() { // Average Displacement
         // TODO...
-
+        double res = 0;
+        List<Car> cars = DataRepo.getCarsWithEngines();
+        res = cars.stream().mapToDouble(x -> x.getEngine().getDisplacment()).average().orElse(0);
+        System.out.printf("%.02f",res);
     }
-
     private static void ex3() { // People Uppercase
         // TODO...
-
+        List<Person> personList = DataRepo.getPeople();
+        personList = personList.stream().map(x -> new Person(x.getId(), x.getFirstName().toUpperCase(), x.getLastName().toUpperCase(), x.getAge()))
+                .collect(Collectors.toList());
+        System.out.println(personList.toString());
     }
 
     private static void ex4() { // Word counter
